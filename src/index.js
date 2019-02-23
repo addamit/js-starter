5f67d4016b8aabb19ed3b28d107620336bfec6e8
addamit <amitsharma103@gmail.com>
2019-02-23 17:20:25 -0500

    commit p5 template

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import {login} from './app.js';
-console.log('test!!!');
+import p5 from 'p5';
+
+console.log('test!!');
+
+const s = (sketch) => {
+
+	var canvasWidth  = 600;
+	var canvasHeight = 600;
+
+
+	sketch.setup = function() {
+		
+		sketch.createCanvas(canvasWidth, canvasHeight);
+		sketch.frameRate(30);
+
+	}
+
+	sketch.draw = function() {
+		sketch.background(100);
+	}
+
+}
+
+
+var myp5 = new p5(s);
